2fd64cca98b6b9e5bcde9271a8ec8c1a5d518d8d
master <master@dib-e026-imac20.dib.upv.es>
2025-03-14 18:20:43 +0100

    Creación de páginas y configuración inicial sin router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hola Mi Mundo</h1>
+  <!--<h1 class="text-3xl font-bold underline">Hola Mi Mundo</h1>-->
+  <DomusPagina />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DomusPagina from './modulorum/landing/paginae/DomusPagina.vue';
+</script>
 
 <style scoped></style>
